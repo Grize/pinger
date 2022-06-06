8f028cfbb419e9e10077a9a9cb2d2d6c06dd23d5
Grize <myheroreadytodie@gmail.com>
2022-06-06 04:15:23 +0300

    Dev

--- ping_service/lib/ping_daemon.rb
+++ ping_service/lib/ping_daemon.rb
@@ -3,30 +3,34 @@
 require_relative 'ping_runner'
 require_relative 'ping_storage'
 require 'concurrent'
+require 'concurrent/atomics'
 
 class PingDaemon
-  attr_reader :db, :influx, :pinger_factory, :thread_pool
+  attr_reader :db, :influx, :pinger_factory, :thread_pool, :aborted
 
   def initialize(db, influx, pinger_factory, pool_size)
     @db = db
     @influx = influx
     @pinger_factory = pinger_factory
     @thread_pool = Concurrent::ThreadPoolExecutor.new(min_threads: 1, max_threads: pool_size)
-    @aborted = false
+    @aborted = Concurrent::AtomicBoolean.new
   end
 
   def run
-    loop do
-      ips = db.list_ips
-      ips.each do |ip|
-        Thread.new { PingRunner.new(influx, pinger_factory, ip).call }.join
+    Thread.new do
+      loop do
+        ips = db.list_ips
+        ips.each do |ip|
+          Thread.new { PingRunner.new(influx, pinger_factory, ip).call }.join
+        end
+        break if @aborted.true?
+
+        sleep(60)
       end
-      break if @aborted
-      sleep(60)
     end
   end
 
   def abort!
-    @aborted = true
+    aborted.make_true
   end
 end
--- ping_service/spec/ping_spec.rb
+++ ping_service/spec/ping_spec.rb
@@ -4,18 +4,39 @@ require_relative '../lib/ping_daemon'
 
 RSpec.describe PingDaemon do
   describe '#run' do
-    let(:ips) { { '64.233.164.102' => 0.40265 } }
     let!(:test_ping_db) { TestPingDb.new(ips.keys) }
     let!(:influx) { TestPingStorage.new('test_connection') }
     let!(:test_ping_factory) { TestPingerFactory.new(ips) }
 
     let(:daemon) { described_class.new(test_ping_db, influx, test_ping_factory, 10) }
 
+    before(:each) do
+      Timecop.freeze
+    end
+
+    after(:each) do
+      Timecop.return
+    end
+
     context 'Daemon send ping' do
-      it 'does ping and send message to influx' do
+      let(:ips) { { '64.233.164.102' => 0.40265 } }
+
+      it 'does ping to required addresses' do
         daemon.abort!
         daemon.run
         expect(test_ping_factory.counter_result.keys).to eq(ips.keys)
+        expect(test_ping_factory.counter_result.values).to eq([1])
+      end
+    end
+
+    context 'Daemon does several iterations' do
+      let(:ips) { { '64.233.164.102' => 0.40265 } }
+
+      it 'does ping to required addresses' do
+        daemon.abort!
+        daemon.run
+        expect(test_ping_factory.counter_result.keys).to eq(ips.keys)
+        expect(test_ping_factory.counter_result.values).to eq([1])
       end
     end
   end
