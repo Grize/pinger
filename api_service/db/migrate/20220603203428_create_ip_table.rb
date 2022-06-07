064c13e20cbce237b7c01cb75611215e677b305e
Grize <myheroreadytodie@gmail.com>
2022-06-07 03:30:05 +0300

    Fix migration and api

--- api_service/db/migrate/20220603203428_create_ip_table.rb
+++ api_service/db/migrate/20220603203428_create_ip_table.rb
@@ -2,12 +2,11 @@
 
 ROM::SQL.migration do
   change do
-    ## TODO => set ip as primary_key
     create_table :ips do
       column :ip, String, null: false
       column :enable, TrueClass, default: true
 
-      primary_key :ip
+      primary_key [:ip]
     end
   end
 end
